bc305eac51696f59f7f7766e6f28ad6cfc7e09f2
Aoi <channelyyt@163.com>
2026-07-26 20:31:20 +0800

    v1.0.1 发布准备

--- aurum_app.py
+++ aurum_app.py
@@ -15,7 +15,7 @@ from aurum_core.utils import open_folder
 from aurum_core.data_io import export_database, import_database
 
 # 当前版本号（每次发布新版本时手动更新）
-CURRENT_VERSION = "1.0.0"  # 请根据实际版本修改
+CURRENT_VERSION = "1.0.1"  # 请根据实际版本修改
 
 # 你的 GitHub 用户名和仓库名
 GITHUB_REPO = "Blue-ringedOctopus/Aurum"  # 替换为你的用户名和仓库名
